34c0e4a7a096875306b2cabe4e18e7f9f52481b5
mia <m.09048030369@gmail.com>
2022-07-31 17:38:05 +1000

    deleted api.js

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApioptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'c80b2ef5ddmsh65e9187bef13d1bp1b2fc4jsnfb8ce9302a0b',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-  },
-};
-
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
